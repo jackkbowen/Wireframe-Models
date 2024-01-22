cc96366df2cde418ab1fa847df281f0246b35f55
jackkbowen <jmbowen@ucsc.edu>
2024-01-22 04:28:10 -0800

    plotting points with background PA0

--- code/webgl.js
+++ code/webgl.js
@@ -15,7 +15,12 @@ var FSHADER_SOURCE =
     '  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Set the point color
     '}\n';
 
+// The array for the position of a mouse press
+var g_points = []; 
+
 function main() {
+    initBackground();
+    
     // Get a reference to the canvas element
     var canvas = document.getElementById('myCanvas');
 
@@ -34,13 +39,6 @@ function main() {
         return;
     }
 
-    // Write the positions of vertices to a vertex shader
-    var n = initVertexBuffers(gl);
-    if (n < 0) {
-        console.log('Failed to set the positions of the vertices');
-        return;
-    }
-    
     // Get the storage location of a_Position
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     if (a_Position < 0) {
@@ -48,63 +46,69 @@ function main() {
         return;
     }
 
-    // Get the number of sides (n) that was entered by the user.
-    var numSides = document.getElementById('numSides').value;
-    var endCaps = document.getElementById('endcapsTF').checked;
-   
-    if (!endCaps){
-        console.log("Not clicked");
-    }
-            
+
     // Register function (event handler) to be called on a mouse press
     canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position); };
-
-    // red green blue alpha
-    gl.clearColor(0.5, 0.5, 0.5, 0.65);
-
-    // clears the drawing area with the specified clear color
-    // gl.COLOR_BUFFER_BIT Specifies the color buffer.
-    // gl.DEPTH_BUFFER_BIT Specifies the depth buffer.
-    // gl.STENCIL_BUFFER_BIT Specifies the stencil buffer.  
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-     // Draw a point
-    gl.drawArrays(gl.LINES, 0, n);
 }
 
 
+var g_points = []; // The array for the position of a mouse press
+function click(ev, gl, canvas, a_Position) {
+  var x = ev.clientX; // x coordinate of a mouse pointer
+  var y = ev.clientY; // y coordinate of a mouse pointer
+  var rect = ev.target.getBoundingClientRect() ;
 
-function initVertexBuffers(gl) {
-    var vertices = new Float32Array([
-        0, .97,   0, -0.97, .97, 0,  -.97, 0
-    ]);
-    var n = 5; // The number of vertices
-  
-    var xAxis = new Float32Array([
-        0, .97,   0, -0.97,   0.0, 0.0
-      ]);
-    // Create a buffer object
-    var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-      console.log('Failed to create the buffer object');
-      return -1;
-    }
-  
-  　// Bind the buffer object to target
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  　// Write date into the buffer object
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-  
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    if (a_Position < 0) {
-      console.log('Failed to get the storage location of a_Position');
-      return -1;
-    }
-    // Assign the buffer object to a_Position variable
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-  
-    // Enable the assignment to a_Position variable
-    gl.enableVertexAttribArray(a_Position);
-  
-    return n;
+  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+  // Store the coordinates to g_points array
+  g_points.push(x); g_points.push(y);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var len = g_points.length;
+  for(var i = 0; i < len; i += 2) {
+    // Pass the position of a point to a_Position variable
+    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+
+    // Draw
+    gl.drawArrays(gl.POINTS, 0, 1);
   }
+}
+
+function initBackground() {
+    // Get a reference to the canvas element
+
+  const canvas = document.getElementById("background");
+
+  //Set the position of the top left of the canvas to be 0,0
+  canvasLeft = canvas.offsetLeft + canvas.clientLeft;
+  canvasTop = canvas.offsetTop + canvas.clientTop;
+
+  // Set default values to check if its the first click
+  let prevX = -1;
+  let prevY = -1;
+  let numDots = -1;
+
+  // For saving the history of the coordinates to output once right click is pressed
+  const historyX = [];
+  const historyY = [];
+
+  // Get a reference to the cursorLocation element
+  const cursor = document.getElementById("cursorLocation");
+
+  // Initialize the 2d context
+  const context = canvas.getContext("2d");
+
+  // Grey Background
+  context.fillStyle = "#cfcdcd";
+  context.fillRect(0, 0, 500, 500);
+
+  // Green Y Axis
+  context.fillStyle = "#257724";
+  context.fillRect(249,5,2,480);
+
+  // Red X Axis
+  context.fillStyle = "#FF0000";
+  context.fillRect(5,249,490,2);
+}
